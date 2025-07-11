3173cd9b09adb91830198ac2d748d3df148ecb45
mauricedeschamps <mementomorikatsu@gmail.com>
2025-07-11 17:15:27 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,7 @@ const ASSETS_TO_CACHE = [
   '/icons/icon-144x144.png',
   '/icons/icon-152x152.png',
   '/icons/icon-192x192.png',
+  '/IMG_20250710_183738_(192_x_192_ピクセル).png',
   '/icons/icon-384x384.png',
   '/icons/icon-512x512.png',
   '/icons/favicon.ico',
